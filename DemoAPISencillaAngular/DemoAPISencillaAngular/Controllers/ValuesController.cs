6cfd370fdcf80f603345220529b4387922bdf0da
Jimmy Sáenz <jsaenzr@outlook.com>
2013-07-15 20:24:06 -0600

    Proyecto de introducción a AngularJS + Web API http://jsaenzr.com/blog/2013/06/06/asp-net-web-api-angularjs-introduccin-i/

---
+++ DemoAPISencillaAngular/DemoAPISencillaAngular/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoAPISencillaAngular.Models;
+
+namespace DemoAPISencillaAngular.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static List<Pais> paises = new List<Pais>()
+            {
+                new Pais(){Id = 1, Nombre = "Nicaragua"},
+                new Pais(){Id = 2, Nombre = "Honduras"},
+                new Pais(){Id = 3, Nombre = "Venezuela"},
+                new Pais(){Id = 4, Nombre = "Colombia"},
+                new Pais(){Id = 5, Nombre = "Argentina"},
+            };
+
+        // GET api/values
+        public IEnumerable<Pais> Get()
+        {
+            return paises;
+        }
+
+        // GET api/values/5
+        public Pais Get(int id)
+        {
+            return paises.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]Pais pais)
+        {
+            paises.Add(pais);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Pais pais)
+        {
+            var item = paises.FirstOrDefault(x => x.Id == id);
+            if (item == null) return;
+            item.Nombre = pais.Nombre;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            paises.Remove(paises.First(x => x.Id == id));
+        }
+    }
+}
---
+++ DemoAPISencillaAngular/DemoAPISencillaAngular/Scripts/app-resource.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("paisesApp", ["ngResource"]);
+
+var mainCtrl = function ($scope, $resource) {
+
+    var paisesService = $resource('/api/values');
+
+    var obtenerPaises = function () {
+        paisesService.query({},
+            function (data) {
+                $scope.paises = data;
+            },
+            function (err) {
+                $scope.error = err;
+            }
+        );
+    };
+
+    obtenerPaises();
+}
